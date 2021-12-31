8d21ca660be57d93983b8444151454eab8b59b51
sommykene <32955277+sommykene@users.noreply.github.com>
2021-12-31 20:54:10 +0000

    added login page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Routes, Route, Link, Outlet } from "react-router-dom";
-import SignUpScreen from "./screens/account/SignUpScreen";
+import LoginScreen from "@screens/account/LoginScreen";
+import SignUpScreen from "@screens/account/SignUpScreen";
 
 export default function App() {
   return (
     <Routes>
       <Route
+        exact
         path="/"
         element={
           <main style={{ padding: "1rem" }}>
@@ -12,10 +14,14 @@ export default function App() {
             <Link style={{ display: "block", margin: "1rem 0" }} to={"/signup"}>
               Signup
             </Link>
+            <Link style={{ display: "block", margin: "1rem 0" }} to={"/login"}>
+              Login
+            </Link>
           </main>
         }
       />
       <Route path="/signup" element={<SignUpScreen />} />
+      <Route path="/login" element={<LoginScreen />} />
     </Routes>
   );
 }
