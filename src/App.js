c24df59b306b77cd51da934ecdc2e63025e82659
sommykene <32955277+sommykene@users.noreply.github.com>
2021-12-31 19:53:05 +0000

    updated signup page route

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Routes, Route, Link, Outlet } from "react-router-dom";
+import SignUpScreen from "./screens/account/SignUpScreen";
 
 export default function App() {
   return (
@@ -8,17 +9,13 @@ export default function App() {
         element={
           <main style={{ padding: "1rem" }}>
             <p>There's nothing here!</p>
+            <Link style={{ display: "block", margin: "1rem 0" }} to={"/signup"}>
+              Signup
+            </Link>
           </main>
         }
       />
-      <Route
-        path="/signup"
-        element={
-          <main style={{ padding: "1rem" }}>
-            <p>There's nothing here!</p>
-          </main>
-        }
-      />
+      <Route path="/signup" element={<SignUpScreen />} />
     </Routes>
   );
 }
