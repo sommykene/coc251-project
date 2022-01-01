712e92deed40954e12341f5c2af835abf8e802e9
sommykene <32955277+sommykene@users.noreply.github.com>
2022-01-01 22:46:29 +0000

    updated route to allow for nested routes

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import { Routes, Route, Link, Outlet } from "react-router-dom";
 import LoginScreen from "@screens/account/LoginScreen";
 import SignUpScreen from "@screens/account/SignUpScreen";
 import Dashboard from "@screens/main/Dashboard";
+import { Box } from "@mui/material";
 
 export default function App() {
   return (
     <Routes>
-      <Route exact path="/" element={<Dashboard />} />
+      <Route path="/*" element={<Dashboard />} />
       <Route path="/signup" element={<SignUpScreen />} />
       <Route path="/login" element={<LoginScreen />} />
     </Routes>
