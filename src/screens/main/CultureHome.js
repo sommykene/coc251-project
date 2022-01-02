7f7daa89890c4fb81d3e94067702be2b683a3b10
sommykene <32955277+sommykene@users.noreply.github.com>
2022-01-02 22:15:24 +0000

    added culture modal onclick

--- src/screens/main/CultureHome.js
+++ src/screens/main/CultureHome.js
@@ -4,6 +4,7 @@ import Stack from "@mui/material/Stack";
 import Paper from "@mui/material/Paper";
 import Masonry from "@mui/lab/Masonry";
 import { styled } from "@mui/material/styles";
+import CultureModal from "@components/CultureModal";
 
 const Label = styled(Paper)(({ theme }) => ({
   ...theme.typography.body2,
@@ -20,16 +21,7 @@ export default function CultureHome() {
     <Box sx={{ width: "100%", minHeight: 829 }}>
       <Masonry columns={5} spacing={3}>
         {itemData.map((item, index) => (
-          <Stack key={index}>
-            <img
-              src={`${item.img}?w=162&auto=format`}
-              srcSet={`${item.img}?w=162&auto=format&dpr=2 2x`}
-              alt={item.title}
-              loading="lazy"
-              style={{ borderTopLeftRadius: 4, borderTopRightRadius: 4 }}
-            />
-            <Label>Traditions</Label>
-          </Stack>
+          <CultureModal item={item} index={index} />
         ))}
       </Masonry>
     </Box>
@@ -40,69 +32,86 @@ const itemData = [
   {
     img: "https://images.unsplash.com/photo-1518756131217-31eb79b20e8f",
     title: "Fern",
+    cat: "nature",
   },
   {
     img: "https://images.unsplash.com/photo-1627308595229-7830a5c91f9f",
     title: "Snacks",
+    cat: "food",
   },
   {
     img: "https://images.unsplash.com/photo-1597645587822-e99fa5d45d25",
     title: "Mushrooms",
+    cat: "food",
   },
   {
     img: "https://images.unsplash.com/photo-1529655683826-aba9b3e77383",
     title: "Tower",
+    cat: "place",
   },
   {
     img: "https://images.unsplash.com/photo-1471357674240-e1a485acb3e1",
     title: "Sea star",
+    cat: "nature",
   },
   {
     img: "https://images.unsplash.com/photo-1558642452-9d2a7deb7f62",
     title: "Honey",
+    cat: "food",
   },
   {
     img: "https://images.unsplash.com/photo-1516802273409-68526ee1bdd6",
     title: "Basketball",
+    cat: "object",
   },
   {
     img: "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
     title: "Breakfast",
+    cat: "food",
   },
   {
     img: "https://images.unsplash.com/photo-1627328715728-7bcc1b5db87d",
     title: "Tree",
+    cat: "nature",
   },
   {
     img: "https://images.unsplash.com/photo-1551782450-a2132b4ba21d",
     title: "Burger",
+    cat: "food",
   },
   {
     img: "https://images.unsplash.com/photo-1522770179533-24471fcdba45",
     title: "Camera",
+    cat: "object",
   },
   {
     img: "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c",
     title: "Coffee",
+    cat: "food",
   },
   {
     img: "https://images.unsplash.com/photo-1627000086207-76eabf23aa2e",
     title: "Camping Car",
+    cat: "vehicle",
   },
   {
     img: "https://images.unsplash.com/photo-1533827432537-70133748f5c8",
     title: "Hats",
+    cat: "object",
   },
   {
     img: "https://images.unsplash.com/photo-1567306301408-9b74779a11af",
     title: "Tomato basil",
+    cat: "food",
   },
   {
     img: "https://images.unsplash.com/photo-1627328561499-a3584d4ee4f7",
     title: "Mountain",
+    cat: "place",
   },
   {
     img: "https://images.unsplash.com/photo-1589118949245-7d38baf380d6",
     title: "Bike",
+    cat: "vehicle",
   },
 ];
