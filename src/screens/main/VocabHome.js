8460f4c87c512b02012344631e97ef40e02cc88a
sommykene <32955277+sommykene@users.noreply.github.com>
2022-01-02 17:25:24 +0000

    added vocab page

---
+++ src/screens/main/VocabHome.js
@@ -0,0 +1,10 @@
+import { Menu } from "@mui/icons-material";
+import { Box } from "@mui/material";
+
+export default function VocabHome() {
+  return (
+    <Box>
+      <h1>Vocab Home</h1>
+    </Box>
+  );
+}
